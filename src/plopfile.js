084308ac17847a9e7ba35c2a2e91238f70cc54d8
Kevin Old <kevin@cypress.io>
2020-07-31 20:43:54 -0500

    initial commit

---
+++ src/plopfile.js
@@ -0,0 +1,30 @@
+module.exports = function (plop) {
+  plop.setGenerator('test', {
+    prompts: [
+      {
+        type: 'confirm',
+        name: 'wantTacos',
+        message: 'Do you want tacos?',
+      },
+    ],
+    actions: function (data) {
+      var actions = [];
+
+      if (data.wantTacos) {
+        actions.push({
+          type: 'add',
+          path: 'folder/{{dashCase name}}.txt',
+          templateFile: 'templates/tacos.txt',
+        });
+      } else {
+        actions.push({
+          type: 'add',
+          path: 'folder/{{dashCase name}}.txt',
+          templateFile: 'templates/burritos.txt',
+        });
+      }
+
+      return actions;
+    },
+  });
+};
