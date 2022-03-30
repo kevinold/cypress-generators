c9e723f83f86af25e182f7d8b6a524aa62f50b76
Robert Guss <robertguss@pm.me>
2022-03-30 12:55:22 -0400

    added login and spec generators

--- src/plopfile.js
+++ src/plopfile.js
@@ -22,9 +22,63 @@ module.exports = function (plop) {
 
       actions.push({
         type: "add",
-        path: `cypress/tests/ui/{{dashCase specName}}.spec.{{ ext }}`,
-        templateFile: `templates/${data.ext}-spec.txt`,
+        path: `cypress/integration/{{dashCase specName}}.spec.{{ ext }}`,
+        templateFile: `templates/spec/spec.js`,
       });
+
+      return actions;
+    },
+  });
+
+  plop.setGenerator("login", {
+    prompts: [
+      {
+        type: "input",
+        name: "specName",
+        message: "What is the name of your spec?",
+      },
+      {
+        type: "list",
+        name: "ext",
+        message: "Which language?",
+        choices: [
+          { name: "JavaScript", value: "js" },
+          { name: "TypeScript", value: "ts" },
+        ],
+        default: "js",
+      },
+      {
+        type: "list",
+        name: "type",
+        message: "Do you want the complete spec or just the scaffolding?",
+        choices: [
+          { name: "Complete", value: "complete" },
+          { name: "Scaffold", value: "scaffold" },
+        ],
+        default: "js",
+      },
+    ],
+    actions: function (data) {
+      const actions = [];
+
+      if (data.type === "complete") {
+        actions.push({
+          type: "add",
+          path: `cypress/integration/{{dashCase specName}}.spec.{{ ext }}`,
+          templateFile: `templates/login/login-complete.js`,
+        });
+        actions.push({
+          type: "append",
+          path: `cypress/support/commands.js`,
+          templateFile: `templates/login/login-command.js`,
+        });
+      } else {
+        actions.push({
+          type: "add",
+          path: `cypress/integration/{{dashCase specName}}.spec.{{ ext }}`,
+          templateFile: `templates/login/login-scaffold.js`,
+        });
+      }
 
       return actions;
     },
