09e05d0f1d5c181b5a114d9251db2d82d3815dd8
Kevin Old <kevin@cypress.io>
2022-03-31 13:59:39 -0500

    add conditional questions

--- src/plopfile.js
+++ src/plopfile.js
@@ -1,6 +1,39 @@
+const generatorTypes = {
+  type: "list",
+  name: "generatorType",
+  message: "What do you want?",
+  choices: [
+    { name: "Spec Scaffold", value: "scaffold" },
+    { name: "Example Spec", value: "examples" },
+    { name: "Custom Command", value: "command" },
+  ],
+};
+
 module.exports = function (plop) {
   plop.setGenerator("spec", {
     prompts: [
+      generatorTypes,
+      {
+        type: "list",
+        name: "theScaffold",
+        message: "What would you like scaffolded?",
+        choices: [
+          { name: "Blank Test File", value: "blank-test-file" },
+          { name: "Login Form", value: "login-form" },
+        ],
+        when: (answers) => answers.generatorType === "scaffold",
+        default: "blank-test-file",
+      },
+      {
+        type: "list",
+        name: "theExample",
+        message: "Which example?",
+        choices: [
+          { name: "Login Form", value: "login-form" },
+          { name: "Auth Tests", value: "auth" },
+        ],
+        when: (answers) => answers.generatorType === "examples",
+      },
       {
         type: "input",
         name: "specName",
@@ -23,7 +56,7 @@ module.exports = function (plop) {
       actions.push({
         type: "add",
         path: `cypress/tests/ui/{{dashCase specName}}.spec.{{ ext }}`,
-        templateFile: `templates/${data.ext}-spec.txt`,
+        templateFile: `templates/${data.generatorType}/${data.ext}-spec.txt`,
       });
 
       return actions;
