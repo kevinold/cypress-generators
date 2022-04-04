36175804c48a21d3e7993f7ce9f6a45a325d7fb5
Robert Guss <robertguss@pm.me>
2022-04-04 16:18:19 -0400

    created an action-mock spec file that uses mock-fs to mock the filesystem in memory instead of writing to disk.

---
+++ tests/actions-mock.spec.js
@@ -0,0 +1,53 @@
+import mock from "mock-fs";
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { render } from "cli-testing-library";
+import * as fs from "node:fs";
+import { readFileSync } from "fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+describe("Scaffold Actions", () => {
+  afterAll(() => {
+    mock.restore();
+  });
+
+  test("the Empty Spec Boilerplate creates the correct file & contents", async () => {
+    const template = fs.readFileSync(
+      resolve(__dirname, "../src/templates/scaffold/empty.js"),
+      "utf8"
+    );
+
+    mock({
+      "cypress/integration": {
+        "empty.js": template,
+      },
+    });
+
+    const { findByText, userEvent } = await render("node", [
+      resolve(__dirname, "../src/cli.js"),
+    ]);
+
+    expect(await findByText("What do you want?")).toBeInTheConsole();
+    userEvent.keyboard("[Enter]");
+    expect(await findByText("Empty Spec Boilerplate")).toBeInTheConsole();
+
+    userEvent.keyboard("[Enter]");
+
+    expect(
+      await findByText("Please enter the file name for the spec.")
+    ).toBeInTheConsole();
+    userEvent.keyboard("empty");
+    userEvent.keyboard("[Enter]");
+
+    expect(
+      await findByText("Are you writing this spec in JavaScript or TypeScript?")
+    ).toBeInTheConsole();
+    userEvent.keyboard("[Enter]");
+
+    const output = `${process.cwd()}/cypress/integration/empty.js`;
+    const result = readFileSync(output, "utf8");
+
+    expect(result).toMatch(template);
+  });
+});
