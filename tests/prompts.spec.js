a1519a252b25ab10b7567be1ed0d6f3d978b2b98
Robert Guss <robertguss@pm.me>
2022-04-04 11:52:18 -0400

    added tests for all prompts and their options

--- tests/prompts.spec.js
+++ tests/prompts.spec.js
@@ -4,16 +4,63 @@ import { fileURLToPath } from "node:url";
 import { render } from "cli-testing-library";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const containingPath = path.resolve(__dirname, "../src");
-
-const foo = render(resolve(containingPath, "plopfile.js"));
-
-// console.log(foo);
 
 test("renders the What do you want? prompt", async () => {
-  const { getByText, findByText } = await render(
-    resolve(containingPath, "cli.js")
-  );
+  const { findByText } = await render("node", [
+    resolve(__dirname, "../src/cli.js"),
+  ]);
 
-  expect(await getByText("What do you want?")).toBeInTheConsole();
+  expect(await findByText("What do you want?")).toBeInTheConsole();
+});
+
+test("the Spec Scaffold prompt renders the correct options", async () => {
+  const { findByText, userEvent } = await render("node", [
+    resolve(__dirname, "../src/cli.js"),
+  ]);
+
+  expect(await findByText("What do you want?")).toBeInTheConsole();
+
+  userEvent.keyboard("[Enter]");
+
+  expect(await findByText("Empty Spec Boilerplate")).toBeInTheConsole();
+  expect(await findByText("Login Form Spec")).toBeInTheConsole();
+  expect(await findByText("API Spec")).toBeInTheConsole();
+  expect(
+    await findByText("Network Spec - uses cy.intercept()")
+  ).toBeInTheConsole();
+});
+
+test("the Example Spec prompt renders the correct options", async () => {
+  const { findByText, userEvent } = await render("node", [
+    resolve(__dirname, "../src/cli.js"),
+  ]);
+
+  expect(await findByText("What do you want?")).toBeInTheConsole();
+
+  userEvent.keyboard("[ArrowDown]");
+
+  userEvent.keyboard("[Enter]");
+
+  expect(await findByText("Login Form Spec")).toBeInTheConsole();
+  expect(await findByText("API Spec")).toBeInTheConsole();
+  expect(
+    await findByText("Network Spec - uses cy.intercept()")
+  ).toBeInTheConsole();
+  expect(await findByText("localStorage")).toBeInTheConsole();
+});
+
+test("the Custom Command prompt renders the correct options", async () => {
+  const { findByText, userEvent } = await render("node", [
+    resolve(__dirname, "../src/cli.js"),
+  ]);
+
+  expect(await findByText("What do you want?")).toBeInTheConsole();
+
+  userEvent.keyboard("[ArrowDown]");
+  userEvent.keyboard("[ArrowDown]");
+
+  userEvent.keyboard("[Enter]");
+
+  expect(await findByText("Login")).toBeInTheConsole();
+  expect(await findByText("localStorage")).toBeInTheConsole();
 });
